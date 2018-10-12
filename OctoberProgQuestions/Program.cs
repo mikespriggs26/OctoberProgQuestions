3eaa3694d03e5e53305da8bace9461241251a44d
mikespriggs26 <mikespriggs@msn.com>
2018-10-12 18:49:25 -0400

    Added some problems

--- OctoberProgQuestions/Program.cs
+++ OctoberProgQuestions/Program.cs
@@ -10,24 +10,59 @@ namespace OctoberProgQuestions
     {
         static void Main(string[] args)
         {
-            string strA;
-            string strB;
-            bool isAnagram = true;
+            //Checks to see if two strings are anagrams
+            //string strA;
+            //string strB;
+            //bool isAnagram = true;
 
-            Console.WriteLine("Please enter a word: ");
-            strA = Console.ReadLine();
-            Console.WriteLine("Please enter another word: ");
-            strB = Console.ReadLine();
-            foreach (char item in strA)
-            {
-                if (strB.Contains(item) == false)
-                {
-                    isAnagram = false;
-                    break;
-                }
-            }
-            Console.WriteLine(isAnagram);
+            //Console.WriteLine("Please enter a word: ");
+            //strA = Console.ReadLine();
+            //Console.WriteLine("Please enter another word: ");
+            //strB = Console.ReadLine();
+            //foreach (char item in strA)
+            //{
+            //    if (strB.Contains(item) == false)
+            //    {
+            //        isAnagram = false;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(isAnagram);
 
+
+            //Remove duplicate characters from a string
+            //Console.WriteLine("Please enter a word");
+            //string userInputB = Console.ReadLine();
+            //string userInputMinusDups = "";
+            //foreach (char item in userInputB)
+            //{
+            //    for (int i = 0; i < userInputB.Length; i++)
+            //    {
+            //        if(!userInputMinusDups.Contains(item))
+            //        {
+            //            userInputMinusDups += item;
+
+            //        }
+
+            //    }
+            //}
+            //Console.WriteLine(userInputMinusDups);
+
+            //Check to see if a number is an Armstrong number
+            //Console.WriteLine("Please enter a number");
+            //string userInputC = Console.ReadLine();
+            //int userInputCInt = int.Parse(userInputC);
+            //int num1 = int.Parse(userInputC[0].ToString());
+            //int num2 = int.Parse(userInputC[1].ToString());
+            //int num3 = int.Parse(userInputC[2].ToString());
+            //if (num1 * num1 * num1 + num2 * num2 * num2 + num3 * num3 * num3 == userInputCInt)
+            //{
+            //    Console.WriteLine("Your number is an Armstrong number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your number is NOT an Armstrong number.");
+            //}
 
             Console.ReadKey();
 
